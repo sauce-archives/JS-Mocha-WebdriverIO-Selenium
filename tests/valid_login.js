29f15ca9348ab947b0ed3a46304e91465e4c7b11
nikolay.advolodkin <nikolay.advolodkin@saucelabs.com>
2019-06-12 11:10:21 -0400

    updated the login test to not have element exposed

--- tests/valid_login.js
+++ tests/valid_login.js
@@ -1,12 +1,14 @@
 const expect = require('chai').expect;
 const LoginPage = require('../pages/login.page');
 const loginPage = new LoginPage();
+const ProductsPage = require('../pages/productsPage');
+const productsPage = new ProductsPage();
 
 describe('Standard user', function() {
 
-    it("login successfully", function() {
+    it("can login successfully", function() {
         loginPage.open();
         loginPage.loginAs("standard_user", "secret_sauce");
-        expect($('#header_container').isDisplayed());
+        expect(productsPage.isLoaded());
     });
 });
